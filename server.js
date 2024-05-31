fd0c1844a7f90bfe79f00f4f4f104c1b3b40060f
cd011 <zerocj00@gmail.com>
2024-05-31 17:48:30 +0530

    until middleware

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-const port = 3000;
+//get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//console.log(__filename, __dirname);
 
-const server = http.createServer((req, res) => {
+const port = process.env.PORT;
+
+const server = http.createServer(async (req, res) => {
   //   res.setHeader("Content-Type", "text/html");
   //   res.statusCode = 404;
-  res.writeHead(500, { "Content-Type": "application/json" });
-  // res.write("<h1>Hello Trav</h1>");
-  res.end(JSON.stringify({ message: "Server Error" }));
-  //res.end();
+  //   res.writeHead(500, { "Content-Type": "application/json" });
+  //   res.end(JSON.stringify({ message: "Server Error" }));
+
+  //   console.log(req.url);
+  //   console.log(req.method);
+
+  try {
+    //check if GET request
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("Not Found");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.write("Server Error");
+    res.end();
+  }
 });
 
 server.listen(port, () => {
